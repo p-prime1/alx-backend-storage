39669600b71bc92cb1bde2d9ee2bdbe2a8b94504
p-prime1 <preciousnwaka95@gmail.com>
2024-10-26 00:23:55 +0100

    Added new methods to the class"

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable, Any
 
 """Module conatains a cache class"""
 
@@ -14,11 +14,54 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Method generates a key sets it to the _redis obeject
+        """Stores data in Redis and returns a generated key.
+
         Args:
             data (str, bytes, int, float): To be passed to the class
-        Return (str): Generated key
+
+        Return 
+            (str): Generated key
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], Any]] = None):
+        """Retrieves the value of the key from Redis and applies an
+        optional conversion func
+
+        Args:
+            key (str): The key to retireve.
+            fn (Optional[Callable[[bytes], Any]): An optional funtion
+                to convert
+
+        Return:
+            Optional[Any]: The converted value if fn is provided or None
+            it doesnt exist
+            """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        return fn(value) if fn else value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """"Retrieves the value as a string
+
+        Args:
+            key (str) : The key to retrive
+
+        Returns:
+            Optional[str]: The value converted to a strign or None if it doesnt exist
+            """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieves the value of the key as a int
+
+        Args:
+            key (int): The key to retrieve
+
+        Returns:
+            Optional[str]: The value converted to a string or None if it doesnt exist
+            """
+        return self.get(key, int)
