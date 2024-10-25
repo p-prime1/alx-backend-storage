4c7d1c57d6c3ce33ebc92b207f7988569df6b5bd
p-prime1 <preciousnwaka95@gmail.com>
2024-10-25 21:04:41 +0100

    first commit

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python 3
+import redis
+import uuid
+from typing import Union
+
+"""Module conatains a cache class"""
+
+class Cache:
+    """A cache class that Simplifies writing to redis"""
+    def __init__(self):
+        """Initializes the redis module"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+    
+    def store(self,data : Union[str, bytes, int, float]) -> str:
+        """Method generates a key sets it to the _redis obeject
+        Args:
+            data (str, bytes, int, float): To be passed to the class
+        Return (str): Generated key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
