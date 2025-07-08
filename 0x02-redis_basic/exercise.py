f6ac2858c9e2ca599bf5f2682d9b09af930ef831
p-prime1 <preciousnwaka95@gmail.com>
2025-07-08 21:12:29 +0100

    Imlemented a decorator that stores the inputs and outputs of the method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,19 +3,35 @@ import redis
 import uuid
 from typing import Union, Optional, Any
 from functools import wraps
+
 """Module to test and practice working with redis in python"""
 
 
 def count_calls(method: Callable) -> Callable:
     """A decorator function"""
+
     @wraps(method)
     def increment(self, *args, **kwargs):
         """Function increments each time the method is called"""
         self._redis.incr(method.__qualname__, 1)
         return method(self, *args, **kwargs)
+
     return increment
 
-class Cache():
+
+def call_history(method: Callable) -> Callable:
+    """A decorator function"""
+
+    @wraps(method)
+    def history(self, *args, **kwargs):
+        self._redis.rpush(method.__qualname__":inputs", str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(method.__qualname__":ouputs", str(output))
+        return output
+    return history
+
+
+class Cache:
     """Cache clase used to store data in a redis-server"""
 
     def __init__(self):
@@ -29,21 +45,18 @@ class Cache():
         self._redis.set(key, data)
         return key
 
-    
     def get(self, key: string, fn: Optional[Callable]) -> Any:
-        """Retrieves the value of the key from Redis, and 
-            applies an optional converstion function"""
+        """Retrieves the value of the key from Redis, and
+        applies an optional converstion function"""
         value = self._redis.get(key)
         if value is None:
             return None
         return fn(value) if fn else value
 
-    
     def get_str(self, key: str) -> Optional[str]:
         """Method converts return value to a string"""
-        return self.get(key, lambda d: d.decode('utf-8'))
+        return self.get(key, lambda d: d.decode("utf-8"))
 
-    
     def get_int(self, key: str) -> Optional[int]:
         """Method converts return value to an int"""
         return self.get(key, lambda d: int(d))
